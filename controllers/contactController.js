8aa8264dd1f4d5cac8cf460976c248efa921d1c3
Kelumsameera <sameerakelum16@gmail.com>
2025-12-31 11:44:45 +0530

    bugfix env

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
+    user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
 });
